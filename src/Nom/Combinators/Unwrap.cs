a55953df5bb4d8ba210ce939de31b336c8e62d1f
Alfonso Suarez <68227851+alfosua@users.noreply.github.com>
2022-04-18 13:14:23 -0400

    feat: unwrap combinator (wip)

---
+++ src/Nom/Combinators/Unwrap.cs
@@ -0,0 +1,37 @@
+﻿namespace Nom.Combinators;
+
+public interface IUnwrapParser<TInput, TParserOutput, TOutput> : IParser<TInput, TOutput>
+    where TInput : IParsable
+    where TParserOutput : IContentVisitable<TOutput>
+{
+}
+
+public class UnwrapParser<TInput, TParserOutput, TOutput> : IUnwrapParser<TInput, TParserOutput, TOutput>
+    where TInput : IParsable
+    where TParserOutput : IContentVisitable<TOutput>
+{
+    public UnwrapParser(IParser<TInput, TParserOutput> parser)
+    {
+        Parser = parser;
+    }
+
+    public IParser<TInput, TParserOutput> Parser { get; }
+
+    public IResult<TInput, TOutput> Parse(TInput input)
+    {
+        var result = Parser.Parse(input);
+        return Result.Create(input, result.Output.VisitContent());
+    }
+}
+
+public static class Unwrap
+{
+    public static IUnwrapParser<TInput, TParserOutput, TOutput>
+        Create<TInput, TParserOutput, TOutput>(
+            IParser<TInput, TParserOutput> parser)
+            where TInput : IParsable
+            where TParserOutput : IContentVisitable<TOutput>
+    {
+        return new UnwrapParser<TInput, TParserOutput, TOutput>(parser);
+    }
+}
